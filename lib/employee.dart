43b19f5dc6ec7d97dd52df7a9dc5bfd1cda290f5
Khunaiko <pasuaiko21089@gmail.com>
2023-11-27 15:13:11 +0700

    first commit

---
+++ lib/employee.dart
@@ -0,0 +1,15 @@
+class Employee{
+  //Properties variable
+  int count = 0;
+
+  //2.Constructor:method instance of Class
+  Employee(){
+  count = count  + 1;
+  }
+
+  //3.Method
+  void totalEmployee(){
+    print(count);
+  }
+
+}
